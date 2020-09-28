19c89cbd8795bffcc58ea48cc7c42bac67d0fffd
ruthvika543 <72019542+ruthvika543@users.noreply.github.com>
2020-09-28 17:10:35 +0530

    Update Printingstrings.c

--- Printingstrings.c
+++ Printingstrings.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char a;
+char s[100];
+char sen[100];
+scanf("%c",&a);
+scanf("%s",&s);
+scanf("\n");
+scanf("%[^\n]%*c",&sen);
+printf("%c\n",a);
+printf("%s\n",s);
+puts(sen);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
