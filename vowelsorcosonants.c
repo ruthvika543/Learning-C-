26af25bc27635a83c2799441243f5715a5b34273
ruthvika543 <72019542+ruthvika543@users.noreply.github.com>
2020-09-29 10:48:19 +0530

    to differentiate vowels and consonants

---
+++ vowelsorcosonants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char var,vowels;
+    scanf("%c",&var);
+    vowels=(a,e,i,o,u);
+    if(var=vowels)
+    printf("%c is a vowel",var);
+    else
+    printf("%c is a consonant",var);
+    return 0;
+    
+}
