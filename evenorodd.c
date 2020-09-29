d94b5be7ec4632e28d5cf42c59d2036a39a68389
ruthvika543 <72019542+ruthvika543@users.noreply.github.com>
2020-09-29 19:23:47 +0530

    corrected the if statement

--- evenorodd.c
+++ evenorodd.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
 int main()
 {
     int num;
     scanf("%d",&num);
-    if(num divisible by 2);
-    printf("%d is even",num);
-    else 
-    printf("%d is odd",num);
+    if(num==0)
+    printf("%d is neither even nor odd",num);
+    else
+    {
+        if(num%2==0)
+        printf("%d is even",num);
+        else
+        printf("%d is odd",num);
+    }
     return 0;
 }
