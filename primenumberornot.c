25b2c4cd875c77277f92bc6b97488b4a46f6c755
ruthvika543 <72019542+ruthvika543@users.noreply.github.com>
2020-09-30 18:54:13 +0530

    used count

--- primenumberornot.c
+++ primenumberornot.c
@@ -2,15 +2,21 @@
 
 int main()
 {
-  int num,i,answer;
+  int num,i,remainder,count=0;
   scanf("%d",&num);
-  for (i=0;i<=num;i++)
-  {
-      answer=num%i;
-  }
-  if (answer==0)
+  remainder=0;
+ for (i=2;i<num;i++)
+ {
+     remainder=num%i;
+     if(remainder==0)
+        count=count;
+     else
+        count++;
+ }
+  if (count==(num-2))
     printf("%d is a prime number",num);
   else
-    printf("%d is not a prime number"num);
+    printf("%d is not a prime number",num);
+
   return 0;
 }
